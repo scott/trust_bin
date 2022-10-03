81c9c37bfda2c0dd5e04753aea499e0fec8ebd97
Scott Miller <scott.miller.utah@gmail.com>
2022-10-03 09:47:08 -0600

    Add Pagination feature, closes #11 (#17)

---
+++ config/initializers/pagy.rb
@@ -0,0 +1,5 @@
+# Require a CSS framework extra in the pagy initializer (e.g. bootstrap)
+require 'pagy/extras/bootstrap'
+
+Pagy::DEFAULT[:items] = 3        # items per page
+# Pagy::DEFAULT[:size]  = [1,4,4,1] # nav bar links
