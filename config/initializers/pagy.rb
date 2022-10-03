48ae75d0e553a13af00f76fdbdf50e2fcfaf0ff6
Scott Miller <scott@helpy.io>
2022-10-03 09:48:56 -0600

    Update pagination amount to 15

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,5 @@
 # Require a CSS framework extra in the pagy initializer (e.g. bootstrap)
 require 'pagy/extras/bootstrap'
 
-Pagy::DEFAULT[:items] = 3        # items per page
+Pagy::DEFAULT[:items] = 15        # items per page
 # Pagy::DEFAULT[:size]  = [1,4,4,1] # nav bar links
